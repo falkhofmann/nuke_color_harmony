71b03e5c7370b95a92cdaa4bcb84936a2fd011fe
falk <falk@kombinat-13b.de>
2023-02-04 00:16:48 +0100

    add value slider and export to clipboard

---
+++ main.py
@@ -0,0 +1,23 @@
+
+import sys
+
+from PySide2 import QtWidgets
+
+from nuke_color_harmony import view
+from nuke_color_harmony.controller import Controller
+
+
+def start_from_main():
+    """Start up function from outside nuke."""
+
+    app = QtWidgets.QApplication(sys.argv)
+    view_ = view.ColorPaletteUi()
+    controller = Controller(view_)
+    controller.view.raise_()
+    controller.view.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    start_from_main()
